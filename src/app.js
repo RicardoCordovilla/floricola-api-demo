bc123321a609d9a517aed1387dbadefaf7d02de4
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-07-31 13:39:54 -0500

    mqtt post

--- src/app.js
+++ src/app.js
@@ -44,3 +44,50 @@ app.use('/api/v1/stations', stationsRouter)
 app.listen(port, () => {
     console.log(`server started at ${port}`)
 })
+
+
+
+
+// ----------  MOSQUITO -----------
+
+// const registersServices = require('../mvc/registers/registers.services')
+
+const { default: axios } = require('axios')
+// const express = require('express')
+// const app = express()
+// app.use(express.json())
+
+const mqtt = require('mqtt')
+const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt')
+
+client.on('connect', function () {
+    client.subscribe('miracleF01/devices', function (err) {
+        // if (!err) {
+        //     client.publish('vite3-notification-test',
+        //         JSON.stringify({ "station": "ESP1", "values": { "H": 74.2, "T": 25.5 }, "createdAt": new Date() })
+        //         // "gdfg"
+        //     )
+
+        // }
+
+    })
+})
+
+client.on('message', async function (topic, message) {
+    // message is Buffer
+    console.log("message recived: ", message.toString())
+    jsonMessage = JSON.parse(message)
+    console.log(jsonMessage)
+
+    jsonMessage.forEach(element => {
+        const body = element
+        axios.post('https://floricola-api-demo-production.up.railway.app/api/v1/registers',
+            body
+        )
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
+    });
+    // client.end()
+})
+
+
