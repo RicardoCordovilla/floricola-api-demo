9f4ace7300dfcc74e47a05c4fb28e9baef8cb2fa
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-08-02 20:17:05 -0500

    US/Eastern

--- src/mvc/registers/registers.controllers.js
+++ src/mvc/registers/registers.controllers.js
@@ -26,7 +26,7 @@ const currentLocalDate = currentDate.toLocaleDateString("es-EC",
 const currentLocalTime = currentDate.toLocaleTimeString("en-US",
     {
         hour: '2-digit', minute: '2-digit', timeStyle: 'short',
-        timeZone: 'America/Bogota'
+        timeZone: 'US/Eastern'
     })
 
 
